19fd119063891411b1c5dfaa3ea5293cf60213bd
Abhilash S <habelash@gmail.com>
2021-01-10 23:35:19 +0530

    notes

--- teachers/views.py
+++ teachers/views.py
@@ -12,7 +12,11 @@ from django.urls import clear_url_caches
 from django.utils.timezone import utc
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.http import HttpResponse
+from io import BytesIO
+from django.views import View
 
 @login_required
 class StaffRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
@@ -113,3 +117,63 @@ def classNotes(request, slug):
         'classnotes': slug+"notes"
     }
     return render(request, 'teachers/notes/index.html', context)
+
+
+def render_to_pdf(template_src, context_dict={}):
+	template = get_template(template_src)
+	html = template.render(context_dict)
+	result = BytesIO()
+	pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+	if not pdf.err:
+		return HttpResponse(result.getvalue(), content_type='application/pdf')
+	return None
+
+
+#Opens up page as PDF
+
+
+def ViewPDF(request, slug):
+    model = ModelSchema.objects.get(name=slug).as_model()
+    objList = model.objects.all().values()
+    fieldNames = list()
+    noEntry = False
+    try:
+        for x in objList[0]:
+            fieldNames.append(x)
+    except Exception as e:
+        noEntry = True
+    context = {
+        'objects': objList,
+        'noEntry': noEntry,
+        'fieldNames': fieldNames,
+        'objectType': slug,
+    }
+    pdf = render_to_pdf('notes/pdf_template.html', context)
+    return HttpResponse(pdf, content_type='application/pdf')
+
+#Automaticly downloads to PDF file
+def DownloadPDF(request,slug):
+    model = ModelSchema.objects.get(name=slug).as_model()
+    print(model)
+    objList = model.objects.all().values()
+    fieldNames = list()
+    noEntry = False
+    try:
+        for x in objList[0]:
+            fieldNames.append(x)
+    except Exception as e:
+        noEntry = True
+    context = {
+        'objects': objList,
+        'noEntry': noEntry,
+        'fieldNames': fieldNames,
+        'objectType': slug,
+    }
+
+    pdf = render_to_pdf('notes/pdf_template.html', context)
+
+    response = HttpResponse(pdf, content_type='application/pdf')
+    filename = "notes.pdf"
+    content = "attachment; filename='%s'" % (filename)
+    response['Content-Disposition'] = content
+    return response
